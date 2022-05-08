eb3969658ad4e9d0a0a173ccf9496e5e97b53a6b
Themrpie <73369706+Themrpie@users.noreply.github.com>
2022-05-08 17:38:31 -0400

    Update script.js
    Adding function to detect body parts

--- script.js
+++ script.js
@@ -1,38 +1,62 @@
 <html>
   <head>
-    <!-- Load TensorFlow.js -->
     <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.2"></script>
-    <!-- Load BodyPix -->
     <script src="https://cdn.jsdelivr.net/npm/@tensorflow-models/body-pix@2.0"></script>
  </head>
 
   <body>
     <img id='image' src='https://i.imgur.com/E52ALln.jpeg' crossorigin='anonymous'/>
-    <canvas id='canvas' />
+    <canvas id='canvas' /></canvas>
+    <canvas id='canvas2' /></canvas>
   
-  <!-- Place your code in the script tag below. You can also use an external .js file -->
   <script>
-    async function loadAndPredict() {
+    async function cutPerson() {
       
-      const img = document.getElementById('image');
+      var img = document.getElementById('image');
 
-      const net = await bodyPix.load();
-      const segmentation = await net.segmentPerson(img);
+      var net = await bodyPix.load();
+      var segmentation = await net.segmentPerson(img);
 
       // The mask image is an binary mask image with a 1 where there is a person and
       // a 0 where there is not.
-      const coloredPartImage = bodyPix.toMask(segmentation);
-      const opacity = 0.7;
-      const flipHorizontal = false;
-      const maskBlurAmount = 0;
-      const canvas = document.getElementById('canvas');
+      var coloredPartImage = bodyPix.toMask(segmentation);
+      var opacity = 0.99;
+      var flipHorizontal = false;
+      var maskBlurAmount = 0;
+      var canvas = document.getElementById('canvas2');
       // Draw the mask image on top of the original image onto a canvas.
       // The colored part image will be drawn semi-transparent, with an opacity of
       // 0.7, allowing for the original image to be visible under.
-      bodyPix.drawMask(canvas, img, coloredPartImage, opacity, maskBlurAmount, flipHorizontal);
+      bodyPix.drawMask(canvas2, img, coloredPartImage, opacity, maskBlurAmount, flipHorizontal);
           
     }
-    loadAndPredict();
+
+    async function bodyParts() {
+      
+      var img = document.getElementById('image');
+    await cutPerson();
+
+    var net = await bodyPix.load();
+    var partSegmentation = await net.segmentMultiPersonParts(img);
+
+    // The colored part image is an rgb image with a corresponding color from the
+    // rainbow colors for each part at each pixel, and black pixels where there is
+    // no part.
+    var coloredPartImage = bodyPix.toColoredPartMask(partSegmentation);
+    var opacity = 0.7;
+    var flipHorizontal = false;
+    var maskBlurAmount = 0;
+    var canvas2 = document.getElementById('canvas');
+    // Draw the colored part image on top of the original image onto a canvas.
+    // The colored part image will be drawn semi-transparent, with an opacity of
+    // 0.7, allowing for the original image to be visible under.
+    bodyPix.drawMask(
+    canvas, img, coloredPartImage, opacity, maskBlurAmount,
+    flipHorizontal);     
+    }
+
+    bodyParts();
+    //cutPerson();
   </script>
   </body>
 </html>
